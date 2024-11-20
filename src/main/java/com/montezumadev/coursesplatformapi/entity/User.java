c4eea934cb360638925684559a9954abac884fdb
IgorMontezuma20 <igorminranda17@gmail.com>
2024-11-20 12:26:18 -0300

    feat: Create relatonships in the user entity

--- src/main/java/com/montezumadev/coursesplatformapi/entity/User.java
+++ src/main/java/com/montezumadev/coursesplatformapi/entity/User.java
@@ -1,18 +1,51 @@
 package com.montezumadev.coursesplatformapi.entity;
 
+import jakarta.persistence.Basic;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+@Entity
+@Table (name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
     private Long userId;
+
+    @Basic
+    @Column(name = "email", nullable = false, length = 45, unique = true)
     private String email;
+
+    @Basic
+    @Column(name = "password", nullable = false, length = 64)
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
     private Set<Role> roles = new HashSet<>();
 
+    @OneToOne(mappedBy = "user")
     private Student student;
+
+    @OneToOne(mappedBy = "user")
     private Instructor instructor;
 
     public User() {
